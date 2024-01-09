554e7e86cd2b6791ac16303213498b61ecd7f08d
bonifacyklocek <karmelek.pl@gmail.com>
2024-01-09 21:27:29 +0100

    bebebe
    opis

---
+++ HotelFlow/Controllers/TestowyKontroler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelFlow.Controllers
+{
+    public class funkcja : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "bruh"
+        };
+    }
+}
