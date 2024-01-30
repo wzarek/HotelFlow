c5bc02cfc98e6a71f077468f00375126b302c8ef
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2024-01-30 22:53:27 +0100

    Minor changes in navbar rendering

--- HotelFlow/ClientApp/src/components/shared/navigation/NavMenu.jsx
+++ HotelFlow/ClientApp/src/components/shared/navigation/NavMenu.jsx
@@ -15,21 +15,17 @@ export const NavMenu = () => {
                 <div className="hidden sm:flex sm:items-center">
                     <NavItem to="/" name='home' />
                     <NavItem to="/find-room" name='znajdź pokój' />
+                    <div className='w-[1em] h-[1em] border-black border-l-[1px]'></div>
                     {
-                      auth.isAuthenticated &&
-
-                      <Link to="/logout" justLoggedOut={true} className="text-gray-800 text-sm font-semibold border-2 border-solid border-blue-900 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600">wyloguj</Link>
+                      auth.isAuthenticated ?
+                        <Link to="/logout" justLoggedOut={true} className="text-gray-800 text-sm font-semibold border-2 border-solid border-blue-900 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600">wyloguj</Link>
+                      :
+                        <div className="hidden sm:flex sm:items-center">
+                          <Link to="/login" className="text-gray-800 text-sm font-semibold hover:text-blue-600 mr-4">logowanie</Link>
+                          <Link to="/register" className="text-gray-800 text-sm font-semibold border-2 border-solid border-blue-900 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600">rejestracja</Link>
+                        </div>
                     }
                 </div>
-                {
-                    !auth.isAuthenticated &&
-
-                    <div className="hidden sm:flex sm:items-center">
-                      <Link to="/login" className="text-gray-800 text-sm font-semibold hover:text-blue-600 mr-4">logowanie</Link>
-                      <Link to="/register" className="text-gray-800 text-sm font-semibold border-2 border-solid border-blue-900 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600">rejestracja</Link>
-                    </div>
-                }
-                
             </div>
           </div>
         </div>
