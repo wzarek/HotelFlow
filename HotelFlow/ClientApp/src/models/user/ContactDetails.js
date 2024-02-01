98419f65e5268d80c657da9afa445646b6584679
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2024-02-01 13:31:00 +0100

    Updated ContactDetails

--- HotelFlow/ClientApp/src/models/user/ContactDetails.js
+++ HotelFlow/ClientApp/src/models/user/ContactDetails.js
@@ -12,3 +12,19 @@ export class ContactDetailsToGet {
         return new ContactDetailsToGet(parsedJSON.userName, parsedJSON.emailAddress, parsedJSON.phoneNumber, parsedJSON.name, parsedJSON.surname)
     }
 }
+
+export class ContactDetailsToSend {
+    constructor(username, email, number, name, surname, isActive) {
+        this.EmailAddress = email
+        this.UserName = username
+        this.Name = name
+        this.Surname = surname
+        this.PhoneNumber = number
+        this.IsActive = isActive
+    }
+
+    static fromJSON(json) {
+        var parsedJSON = JSON.parse(json)
+        return new ContactDetailsToGet(parsedJSON.userName, parsedJSON.emailAddress, parsedJSON.phoneNumber, parsedJSON.name, parsedJSON.surname, parsedJSON.isActive)
+    }
+}
